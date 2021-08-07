f2a07577ebbd1d13491eb69732f6391cc67dddee
Crowdbotics <team@crowdbotics.com>
2021-08-07 14:38:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "red_band_29608Identifier",
+  url: "https://red_band_29608Identifier.botics.co",
+  api: "https://red_band_29608Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
